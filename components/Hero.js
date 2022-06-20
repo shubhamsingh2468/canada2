a6cf5c6b36a3e7c900f36abcf900d280d32538b2
shubhamsingh259 <59261597+shubhamsingh259@users.noreply.github.com>
2022-06-20 11:46:11 +0530

    first commit

--- components/Hero.js
+++ components/Hero.js
@@ -92,8 +92,8 @@ export default function Example() {
                   className="h-8 w-auto sm:h-10"
                   src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                   alt=""
-                  width={1310}
-                  height={873}
+                  width={100}
+                  height={150}
                   
                 />
               </a>
@@ -286,8 +286,9 @@ export default function Example() {
                       className="h-8 w-auto"
                       src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                       alt="Workflow"
-                      width={1310}
-                      height={873}
+                      width={50}
+                      height={200}
+
                     />
                   </div>
                   <div className="-mr-2">
--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["tailwindui.com", "images.unsplash.com"],
+  },
+}
